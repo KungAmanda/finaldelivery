dd6471706c8461758e0865b546db974319dd46b6
kungamanda <amanda-leia.azmoudeh@student.nbi-handelsakademin.se>
2023-03-10 10:24:03 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finaldelivery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your order has now been placed and you will soon get your groceries!" +
+              "You may now exit the page!");
+        }
+    }
+}
