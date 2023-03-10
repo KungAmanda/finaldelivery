9ab953673c0c3262bc8d2d0a88b9c396b58aba3a
kungamanda <amanda-leia.azmoudeh@student.nbi-handelsakademin.se>
2023-03-10 10:48:51 +0100

    final cut

--- Form1.cs
+++ Form1.cs
@@ -19,10 +19,20 @@ namespace finaldelivery
 
         private void button7_Click(object sender, EventArgs e)
         {
+           // if sats som kollar så att alla textboxar är i fyllda
+
+            if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtAdd.Text) || string.IsNullOrEmpty(txtCon.Text))
+            {
+                MessageBox.Show("Vänligen fyll i alla rader", "Felmeddelande", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // om alla textboxar är i fyllda så läggs order och detta visas 
+
             MessageBox.Show("Your order has now been placed and you will soon get your groceries!" +
-              "You may now exit the page!");
-
-
+             "You may now exit the page!");
+            
         }
     }
+    
 }
